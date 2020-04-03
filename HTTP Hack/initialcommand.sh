d08b1462c5154c99e1327a184e7abadb054ab89c
Ivan Hornung <ivanhor2003@gmail.com>
2020-04-03 16:29:19 -0700

    Create initialcommand.sh

---
+++ HTTP Hack/initialcommand.sh
@@ -0,0 +1,11 @@
+#--warning: insecure network connection--
+
+#>>telnet domain port #
+
+telnet www.dr-chuck.com 80
+
+#safe webserver
+
+#Trying 74.208.28.177..
+#Connecting to www.dr-chuck.com.
+#Escape character is '^]'.
