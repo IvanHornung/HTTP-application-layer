d1a2857e591a0c60a87ae2e21f576ef939d50886
Ivan Hornung <ivanhor2003@gmail.com>
2020-04-03 16:43:06 -0700

    Create hacking-http.sh

---
+++ HTTP Hack/hacking-http.sh
@@ -0,0 +1,5 @@
+#GET request: HTTP request/response cycle
+GET http://www.dr-chuck.com/page1.htm HTTP/1.0
+
+#Application layer | Web, File Transfer, E-Mail| > Transport Layer (TCP) | Reliable Connections>
+#Internetwork Layer (IP) | Simple, Unreliable| > Link Layer (Ethernet, WiFi) |Physical Connections|
