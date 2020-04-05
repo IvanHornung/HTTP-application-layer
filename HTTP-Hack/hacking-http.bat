fdbca73e66cf655ea0d999d264fe0e74dbc06ba2
Ivan Hornung <ivanhor2003@gmail.com>
2020-04-04 22:26:19 -0700

    Update and rename hacking-http.sh to hacking-http.bat

--- HTTP-Hack/hacking-http.bat
+++ HTTP-Hack/hacking-http.bat
@@ -3,3 +3,4 @@ GET http://www.dr-chuck.com/page1.htm HTTP/1.0
 
 #Application layer | Web, File Transfer, E-Mail| > Transport Layer (TCP) | Reliable Connections>
 #Internetwork Layer (IP) | Simple, Unreliable| > Link Layer (Ethernet, WiFi) |Physical Connections|
+ 
